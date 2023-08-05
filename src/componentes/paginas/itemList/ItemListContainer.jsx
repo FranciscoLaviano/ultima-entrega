18c3680e2c74569068cfe0e311619e524fcee154
Francisco Laviano <linespi39@gmail.com>
2023-08-05 14:56:44 -0300

    actualizando archivos

--- src/componentes/paginas/itemList/ItemListContainer.jsx
+++ src/componentes/paginas/itemList/ItemListContainer.jsx
@@ -5,25 +5,29 @@ import { getDocs, collection } from "firebase/firestore";
 import ItemList from "./ItemList";
 
 const ItemListContainer = () => {
-  let consulta;
-  let coleccionProductos = collection(db, "productos");
-  if (!categoryName) {
-    consulta = coleccionProductos;
-  } else {
-    consulta = query(coleccionProductos, where("category", "==", categoryName));
-  }
   const [items, setItems] = useState([]);
   const { categoryName } = useParams();
 
   useEffect(() => {
+    let consulta;
+    let coleccionProductos = collection(db, "productos");
+    if (!categoryName) {
+      consulta = coleccionProductos;
+    } else {
+      consulta = query(
+        coleccionProductos,
+        where("category", "==", categoryName)
+      );
+    }
+
     getDocs(consulta).then((res) => {
       let aP = res.docs.map((e) => {
-        return { ...producto.data(), id: producto.id };
+        return { ...e.data(), id: e.id };
       });
       console.log(aP);
       setItems(aP);
     });
-  }, categoryName);
+  }, [categoryName]);
 
   return (
     <>
